b0a4161b67310c40e616ab6828b4b76c86b64eab
Giuseppe Sucameli <brush.tyler@gmail.com>
2011-12-10 02:31:19 +0100

    increment version number

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ def description():
 	return "Creates a dockable map canvas synchronized with the main one"
 
 def version():
-	return "0.1.1"
+	return "0.1.2"
 
 def qgisMinimumVersion():
 	return "1.5.0"
