0eef73dfa586a34ae72db2cb787e3cb965a06563
Martin Dobias <wonder.sk@gmail.com>
2015-07-07 14:51:47 +0200

    Add the possibility to choose layer's preset

--- mirrorMap.py
+++ mirrorMap.py
@@ -68,7 +68,7 @@ class MirrorMap(QWidget):
 		action = settings.value( "/qgis/wheel_action", 0, type=int)
 		zoomFactor = settings.value( "/qgis/zoom_factor", 2.0, type=float )
 		self.canvas.setWheelAction( QgsMapCanvas.WheelAction(action), zoomFactor )
-		gridLayout.addWidget( self.canvas, 0, 0, 1, 5 )
+		gridLayout.addWidget( self.canvas, 0, 0, 1, 6 )
 
 		self.addLayerBtn = QToolButton(self)
 		#self.addLayerBtn.setToolButtonStyle( Qt.ToolButtonTextBesideIcon )
@@ -76,6 +76,7 @@ class MirrorMap(QWidget):
 		self.addLayerBtn.setIcon( QIcon(":/plugins/DockableMirrorMap/icons/plus.png") )
 		QObject.connect(self.addLayerBtn, SIGNAL( "clicked()" ), self.addLayer)
 		gridLayout.addWidget( self.addLayerBtn, 1, 0, 1, 1 )
+		self.addLayerBtn.setAutoRaise(True)
 
 		self.delLayerBtn = QToolButton(self)
 		#self.delLayerBtn.setToolButtonStyle( Qt.ToolButtonTextBesideIcon )
@@ -83,16 +84,25 @@ class MirrorMap(QWidget):
 		self.delLayerBtn.setIcon( QIcon(":/plugins/DockableMirrorMap/icons/minus.png") )
 		QObject.connect(self.delLayerBtn, SIGNAL( "clicked()" ), self.delLayer)
 		gridLayout.addWidget( self.delLayerBtn, 1, 1, 1, 1 )
+		self.delLayerBtn.setAutoRaise(True)
+
+		self.styleMenu = QMenu(self)
+		self.styleBtn = QPushButton("Layer Style", self)
+		self.styleBtn.setMenu(self.styleMenu)
+		self.styleBtn.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
+		gridLayout.addWidget(self.styleBtn, 1, 2, 1, 1)
+		if QGis.QGIS_VERSION_INT < 21100:
+			self.styleBtn.hide()
 
 		self.renderCheck = QCheckBox( "Render", self )
 		QObject.connect(self.renderCheck, SIGNAL( "toggled(bool)" ), self.toggleRender)
 		self.renderCheck.setChecked(True)
-		gridLayout.addWidget( self.renderCheck, 1, 2, 1, 1 )
+		gridLayout.addWidget( self.renderCheck, 1, 3, 1, 1 )
 
 		self.scaleFactorLabel = QLabel(self)
 		self.scaleFactorLabel.setText("Scale factor:")
 		self.scaleFactorLabel.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
-		gridLayout.addWidget(self.scaleFactorLabel, 1, 3, 1, 1)
+		gridLayout.addWidget(self.scaleFactorLabel, 1, 4, 1, 1)
 		self.scaleFactor = QDoubleSpinBox(self)
 		self.scaleFactor.setMinimum(0.0)
 		self.scaleFactor.setMaximum(1000.0)
@@ -100,7 +110,7 @@ class MirrorMap(QWidget):
 		self.scaleFactor.setValue(1)
 		self.scaleFactor.setObjectName("scaleFactor")
 		self.scaleFactor.setSingleStep(.05)
-		gridLayout.addWidget(self.scaleFactor, 1, 4, 1, 1)
+		gridLayout.addWidget(self.scaleFactor, 1, 5, 1, 1)
 		self.scaleFactor.valueChanged.connect(self.onExtentsChanged)
 
 		# Add a default pan tool
@@ -142,7 +152,8 @@ class MirrorMap(QWidget):
 	def refreshLayerButtons(self):
 		has_layers_to_add = False
 		has_layers_to_remove = False
-		for layerId in self._selectedLayers():
+		sel_layers = self._selectedLayers()
+		for layerId in sel_layers:
 			if layerId in self.layers:
 				has_layers_to_remove = True
 			else:
@@ -151,6 +162,31 @@ class MirrorMap(QWidget):
 		self.addLayerBtn.setEnabled(has_layers_to_add)
 		self.delLayerBtn.setEnabled(has_layers_to_remove)
 
+		if QGis.QGIS_VERSION_INT >= 21100:
+			if len(sel_layers) == 1 and sel_layers[0] in self.layers:
+				layer = QgsMapLayerRegistry.instance().mapLayer(sel_layers[0])
+				self._populateLayerStylesMenu(layer)
+			else:
+				self._populateLayerStylesMenu(None)
+
+	def setLayerStyle(self):
+		sel_layers = self._selectedLayers()
+		if len(sel_layers) != 1:
+			return
+		layer = QgsMapLayerRegistry.instance().mapLayer(sel_layers[0])
+
+		styleName = self.sender().text()
+		if styleName == "(default)":
+			styleName = ""
+		overrides = self.canvas.layerStyleOverrides()
+		if styleName == "(use current)":
+			if layer.id() in overrides:
+				del overrides[layer.id()]
+		else:
+			overrides[layer.id()] = layer.styleManager().style(styleName).xmlData()
+		self.canvas.setLayerStyleOverrides(overrides)
+		self.canvas.refresh()
+		self.refreshLayerButtons()
 
 	def getLayerSet(self):
 		return self.layers.keys()
@@ -211,3 +247,33 @@ class MirrorMap(QWidget):
 	def _selectedLayers_old(self):
 		layer = self.iface.activeLayer()
 		return [layer.id()] if layer else []
+
+	def _currentStyleName(self, layer):
+		overrides = self.canvas.layerStyleOverrides()
+		if layer.id() not in overrides:
+			return "__current__"  # special value if not overridden
+
+		for style_name in layer.styleManager().styles():
+			if layer.styleManager().style(style_name).xmlData() == overrides[layer.id()]:
+				return style_name
+
+	def _populateLayerStylesMenu(self, layer):
+		if layer is None or len(layer.styleManager().styles()) <= 1:
+			self.styleBtn.setEnabled(False)
+			return
+
+		self.styleBtn.setEnabled(True)
+		cur_style_name = self._currentStyleName(layer)
+		self.styleMenu.clear()
+
+		a = self.styleMenu.addAction("(use current)", self.setLayerStyle)
+		a.setCheckable(True)
+		a.setChecked(cur_style_name == "__current__")
+		self.styleMenu.addSeparator()
+
+		for style_name in layer.styleManager().styles():
+			is_current = style_name == cur_style_name
+			if len(style_name) == 0: style_name = "(default)"
+			a = self.styleMenu.addAction(style_name, self.setLayerStyle)
+			a.setCheckable(True)
+			a.setChecked(is_current)
